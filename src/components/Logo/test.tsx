55eb2d3c5a0fe2005f2c69b71b5483109da5455f
Diego Buriti <bs.diego97@gmail.com>
2021-01-04 20:55:34 -0300

    add size test by default with normal size

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -10,10 +10,25 @@ describe('<Logo />', () => {
       color: '#fafafa'
     })
   })
+
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
   })
+
+  it('should render logo by default normal size', () => {
+    renderWithTheme(<Logo color="black" size="normal" />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  /*  it('should render logo large size', () => {
+    renderWithTheme(<Logo color="black" size="large" />)
+    expect(screen.getByLabelText(/won games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  }) */
 })
