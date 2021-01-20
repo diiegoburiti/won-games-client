7e0a5d609ff5578b95fe7f12207832b80e7877a0
Diego Buriti <bs.diego97@gmail.com>
2021-01-20 18:29:44 -0300

    chore: add onClick event as a prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,15 +5,17 @@ export type ButtonProps = {
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: React.ReactNode
+  onclick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
   children,
   icon,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
