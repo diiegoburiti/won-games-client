275d8e774893c809aa11e879dbbd14d2436d0346
Diego Buriti <bs.diego97@gmail.com>
2021-01-20 18:17:11 -0300

    chore: add prop icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,14 +4,17 @@ export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
+  icon?: React.ReactNode
 }
 
 const Button = ({
   children,
+  icon,
   size = 'medium',
   fullWidth = false
 }: ButtonProps) => (
-  <S.Wrapper size={size} fullWidth={fullWidth}>
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+    {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
