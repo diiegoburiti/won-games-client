56ac3096c0f593f61e77dbaa4b8926dfd94f1183
Diego Buriti <bs.diego97@gmail.com>
2021-01-27 17:54:15 -0300

    chore: add  menu to open on click event

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,28 +1,33 @@
+import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import * as S from './styles'
 import Logo from 'components/Logo'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
-      </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open shopping cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    <S.MenuFull aria-hidden="true"></S.MenuFull>
-  </S.Wrapper>
-)
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
+      </S.IconWrapper>
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}></S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
