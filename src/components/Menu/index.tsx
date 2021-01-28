331d326a7e4dbb6ada245927e62701b311de1cf3
Diego Buriti <bs.diego97@gmail.com>
2021-01-28 08:56:46 -0300

    chore: add full mobile menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,8 +3,9 @@ import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseMenu } from '@styled-icons/material-outlined/Close'
-import * as S from './styles'
 import Logo from 'components/Logo'
+import Button from 'components/Button'
+import * as S from './styles'
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -28,6 +29,20 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseMenu aria-label="Close menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
