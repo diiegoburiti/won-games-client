9c2849a3ab79cd765ed51eee1d2a41077e7d2335
Diego Buriti <bs.diego97@gmail.com>
2021-02-01 08:30:14 -0300

    chore: add desktop menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -6,6 +6,7 @@ import { Close as CloseMenu } from '@styled-icons/material-outlined/Close'
 import Logo from 'components/Logo'
 import Button from 'components/Button'
 import * as S from './styles'
+import MediaMatch from 'components/MediaMatch'
 
 export type MenuProps = {
   userName?: string
@@ -16,30 +17,46 @@ const Menu = ({ userName }: MenuProps) => {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="Open Menu" />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
         </S.IconWrapper>
         <S.IconWrapper>
-          <ShoppingCartIcon aria-label="Open shopping cart" />
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
         </S.IconWrapper>
+        {!userName && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
-        <CloseMenu aria-label="Close menu" onClick={() => setIsOpen(false)} />
+        <CloseMenu aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
 
           {!!userName && (
             <>
-              <S.MenuLink href="#">My acccount</S.MenuLink>
+              <S.MenuLink href="#">My account</S.MenuLink>
               <S.MenuLink href="#">Wishlist</S.MenuLink>
             </>
           )}
@@ -50,6 +67,7 @@ const Menu = ({ userName }: MenuProps) => {
             <Button fullWidth size="large">
               Log in now
             </Button>
+            <span>or</span>
             <S.CreateAccount href="#" title="Sign Up">
               Sign Up
             </S.CreateAccount>
