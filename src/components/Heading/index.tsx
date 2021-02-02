1f2271e8453e3c8b3143a8090320efa67798a848
Diego Buriti <bs.diego97@gmail.com>
2021-02-02 20:40:34 -0300

    chore : add  prop size

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -5,15 +5,22 @@ export type HeadingProps = {
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
