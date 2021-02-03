af3ceb4c9b92c61c3a6d12de3994c203278ef465
Diego Buriti <bs.diego97@gmail.com>
2021-02-02 21:14:54 -0300

    chore : add props LineColors

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,11 +1,14 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
   size?: 'small' | 'medium'
+  lineColor?: LineColors
 }
 
 const Heading = ({
@@ -13,13 +16,15 @@ const Heading = ({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
-  size = 'medium'
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => (
   <S.Wrapper
     color={color}
     lineLeft={lineLeft}
     lineBottom={lineBottom}
     size={size}
+    lineColor={lineColor}
   >
     {children}
   </S.Wrapper>
