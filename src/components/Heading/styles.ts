0059682a4d7f42ad6044329a5f39cc71f01c593f
Diego Buriti <bs.diego97@gmail.com>
2021-02-02 21:15:13 -0300

    style : fix lineColort params

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -43,8 +43,8 @@ export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
 
-    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor)}
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
     ${!!size && wrapperModifiers[size](theme)}
   `}
 `
