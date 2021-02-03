555ef2e2751a02dfdd3965f787f37cf37db8c089
Diego Buriti <bs.diego97@gmail.com>
2021-02-02 21:04:26 -0300

    style : add tests to change color when prop primary or secondary is passed

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -4,28 +4,28 @@ import { renderWithTheme } from 'components/utils/tests/helpers'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render a white Heading by default', () => {
+  it('should render a white heading by default', () => {
     renderWithTheme(<Heading>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       color: '#FAFAFA'
     })
   })
 
-  it('should render a black Heading when props black is passed', () => {
+  it('should render a black heading when color is passed', () => {
     renderWithTheme(<Heading color="black">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       color: '#030517'
     })
   })
 
-  it('should render a Heading with a line to the left side when props lineLeft is passed', () => {
+  it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
-  it('should render a Heading with a line line bottom when props lineBotton is passed', () => {
+  it('should render a heading with a line at the bottom', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
       'border-bottom',
@@ -36,12 +36,8 @@ describe('<Heading />', () => {
     )
   })
 
-  it('should render a Heading with a small size', () => {
-    renderWithTheme(
-      <Heading lineBottom size="small">
-        Won Games
-      </Heading>
-    )
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       'font-size': '1.6rem'
     })
@@ -53,5 +49,33 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
