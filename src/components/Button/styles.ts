821851a41f1f99d8e29e7edc60314552453c7009
Diego Buriti <bs.diego97@gmail.com>
2021-02-04 11:51:36 -0300

    style: change background button on hover

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -45,6 +45,10 @@ export const Wrapper = styled.button<WrapperProps>`
     padding: ${theme.spacings.xxsmall};
     cursor: pointer;
 
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+    }
+
     ${!!size && wrapperModifiers[size](theme)};
     ${!!fullWidth && wrapperModifiers.fullWidth()}
     ${!!hasIcon && wrapperModifiers.withIcon(theme)}
