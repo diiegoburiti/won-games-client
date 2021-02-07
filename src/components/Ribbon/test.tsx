ea048d26c297f963bdb42b236af11f58adc85de7
Diego Buriti <bs.diego97@gmail.com>
2021-02-07 18:53:04 -0300

    test: add test to render Ribbon component with primary color

--- src/components/Ribbon/test.tsx
+++ src/components/Ribbon/test.tsx
@@ -10,13 +10,15 @@ describe('<Ribbon />', () => {
     expect(screen.getByText(/best seller/i)).toBeInTheDocument()
   })
 
-  /* it('should render with primary color', () => {
+  it('should render with primary color', () => {
     renderWithTheme(<Ribbon color="primary">Best Seller</Ribbon>)
 
     expect(screen.getByText(/best seller/i)).toHaveStyle({
       backgroundColor: '#F231A5'
     })
   })
+
+  /*
 
   it('should render with secondary color', () => {
     renderWithTheme(<Ribbon color="secondary">Best Seller</Ribbon>)
