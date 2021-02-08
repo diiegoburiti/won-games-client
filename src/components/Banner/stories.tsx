87e66a7e5982d28eec7cb3ade2268ee0bf9834d8
Diego Buriti <bs.diego97@gmail.com>
2021-02-08 08:59:28 -0300

    docs: add Ribbon on Banner component

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -17,3 +17,15 @@ export default {
 } as Meta
 
 export const Default: Story<BannerProps> = (args) => <Banner {...args} />
+
+export const withRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Banner {...args} />
+  </div>
+)
+
+withRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
