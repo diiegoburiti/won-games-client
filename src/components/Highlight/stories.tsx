874a481cb42303af44ec3b8cdbaa00b2d4abeb33
Diego Buriti <bs.diego97@gmail.com>
2021-02-09 08:32:43 -0300

    docs: add storybook Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Red Dead it´s back',
+    subtitle: 'Come see jhon`s adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rd2'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
