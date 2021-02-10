dab2048a60c561c3015f003e1484b202ea4edc27
Diego Buriti <bs.diego97@gmail.com>
2021-02-10 09:31:09 -0300

    test: add tests  render right when prop aligment right is passed

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'components/utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'heading 1',
@@ -42,6 +43,30 @@ describe('<Highlight />', () => {
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       '/float-img.png'
+    )
+  })
+
+  it('should render floaImage aling left by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatImage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render floaImage aling right when alingment is passed', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alingment="right" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatImage content'"
     )
   })
 })
