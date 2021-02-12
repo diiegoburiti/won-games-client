396cba00709d268b455475d0ee3926f72857dcb2
Diego Buriti <bs.diego97@gmail.com>
2021-02-12 08:58:20 -0300

    docs: add Ribbon component

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -12,7 +12,8 @@ export default {
     promotionalPrice: 'R$ 200,00'
   },
   argTypes: {
-    onFav: { action: 'clicked' }
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
   },
   parameters: {
     backgrounds: {
@@ -26,3 +27,15 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSizes: 'small',
+  ribbonColors: 'primary'
+}
