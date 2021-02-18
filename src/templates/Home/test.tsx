7007af03908ac7803e6f6e689fa701071585b633
Diego Buriti <bs.diego97@gmail.com>
2021-02-18 08:45:00 -0300

    test: add test to render Menu

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,13 +1,44 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Home from '.'
 
 describe('<Home />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Home />)
+  it('should render Menu', () => {
+    renderWithTheme(<Home />)
 
-    expect(screen.getByRole('heading', { name: /Home/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
   })
+
+  it('should render Footer', () => {
+    renderWithTheme(<Home />)
+
+    expect(
+      screen.getByRole('heading', { name: /contact us/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument()
+  })
+
+  /* it('should render Heading', () => {
+    renderWithTheme(<Home />)
+
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
+    ).toBeInTheDocument()
+  }) */
 })
