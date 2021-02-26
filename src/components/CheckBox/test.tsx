f24bc0ca79d9bdeec3340eada0b6e3d792bb7d65
Diego Buriti <bs.diego97@gmail.com>
2021-02-26 08:42:30 -0300

    test: add test to render with color when prop labelColor is passed

--- src/components/CheckBox/test.tsx
+++ src/components/CheckBox/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import CheckBox from '.'
@@ -6,6 +7,7 @@ import CheckBox from '.'
 describe('<CheckBox />', () => {
   it('should render with label', () => {
     renderWithTheme(<CheckBox label="checkbox label" labelFor="check" />)
+
     expect(screen.getByRole('checkbox')).toBeInTheDocument()
     expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
     expect(screen.getByText(/check/i)).toHaveAttribute('for', 'check')
@@ -14,6 +16,16 @@ describe('<CheckBox />', () => {
   it('should render without label', () => {
     renderWithTheme(<CheckBox />)
 
-    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+    expect(screen.queryByLabelText('checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <CheckBox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
