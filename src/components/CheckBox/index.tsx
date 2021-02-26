581be7ab942e2df9a9345e99864c7eb4cf5a3691
Diego Buriti <bs.diego97@gmail.com>
2021-02-26 08:03:27 -0300

    chore: add inital CheckBox component

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+export type CheckBoxProps = {
+  label: string
+  labelFor: string
+}
+
+const CheckBox = ({ label, labelFor = '' }: CheckBoxProps) => (
+  <S.Wrapper>
+    <input id={labelFor} type="checkbox" />
+    {!!label && <label htmlFor={labelFor}>{label}</label>}
+  </S.Wrapper>
+)
+
+export default CheckBox
