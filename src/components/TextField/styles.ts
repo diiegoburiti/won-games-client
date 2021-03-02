9d5b41e70a6eb6b69a6426f78266cc4426825ec2
Diego Buriti <bs.diego97@gmail.com>
2021-03-01 21:41:46 -0300

    style: add intial TextField stlyes

---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,37 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.section``
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    background: ${theme.colors.lightGray};
+    border-radius: 0.2rem;
+    padding: 0 ${theme.spacings.xsmall};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.lightGray};
+
+    &:focus-within {
+      box-shadow: 0 0 0 0.5rem ${theme.colors.primary};
+    }
+  `}
+`
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    width: 100%;
+    color: ${theme.colors.black};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} 0;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+  `}
+`
+export const Label = styled.label`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    cursor: pointer;
+  `}
+`
