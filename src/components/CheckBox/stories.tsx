e12f983d0b5695496a94ba235655487452ad27cc
Diego Buriti <bs.diego97@gmail.com>
2021-03-02 08:38:18 -0300

    docs: separete Form component

--- src/components/CheckBox/stories.tsx
+++ src/components/CheckBox/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import CheckBox, { CheckBoxProps } from '.'
 
 export default {
-  title: 'CheckBox',
+  title: 'Form/CheckBox',
   component: CheckBox,
   parameters: {
     backgrounds: {
