2bdaa9588500bc74c121e7ee5c732be912c6807f
Diego Buriti <bs.diego97@gmail.com>
2021-03-01 22:18:09 -0300

    test: add new test to render correctly when prop icon is passed

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,5 +1,6 @@
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import TextField from '.'
@@ -62,5 +63,11 @@ describe('<TextField />', () => {
 
     userEvent.tab()
     expect(input).toHaveFocus()
+  })
+
+  it('should with icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId(/icon/i)).toBeInTheDocument()
   })
 })
