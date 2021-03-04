72bb3ec1ce2348e9906ea4141002e6097a37eeb5
Diego Buriti <bs.diego97@gmail.com>
2021-03-03 22:27:06 -0300

    test: add some tests to render Auth correctly

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -4,13 +4,29 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the logos, children, title ', () => {
-    renderWithTheme(<Auth title="Welcome">Enjoi it</Auth>)
+  it('should render the logos, children, title, subtitle, footer', () => {
+    renderWithTheme(
+      <Auth title="Welcome to our page">
+        <span>Enjoy it </span>
+      </Auth>
+    )
 
     expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+
     expect(
-      screen.getByRole('heading', { name: /welcome/i })
+      screen.getByRole('heading', { name: /Welcome to our page/i })
     ).toBeInTheDocument()
-    expect(screen.getByText(/enjoi it/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /WOM is the best and most complete gaming plataform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByText(/Won Games 2020 - 2021 © All rights reserved./i)
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(/enjoy it/i)).toBeInTheDocument()
   })
 })
