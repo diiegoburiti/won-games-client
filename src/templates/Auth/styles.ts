d954d55cbe46d96d0da0c986e1f370935a48c517
Diego Buriti <bs.diego97@gmail.com>
2021-03-03 22:15:36 -0300

    style: add initial Auth styles

---
+++ src/templates/Auth/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const SubTitle = styled.h3``
+
+export const Footer = styled.span``
+
+export const Content = styled.div``
