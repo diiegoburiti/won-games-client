8a9cfbe00756f4b6100c6d60d43951870f40c430
Diego Buriti <bs.diego97@gmail.com>
2021-03-05 07:44:57 -0300

    style: add font huge modifier

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -19,6 +19,14 @@ const wrapperModifiers = {
     `}
   `,
 
+  huge: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.huge};
+
+    ${media.lessThan('medium')`
+      font-size: ${theme.font.sizes.xlarge};
+    `}
+  `,
+
   lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors[lineColor]};
