d683a75400d88bf08b152255f67cc7e6c9527e02
Diego Buriti <bs.diego97@gmail.com>
2021-03-05 10:02:59 -0300

    test: add some test to render FormSignIn correctly

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from '.'
+
+describe('<FormSignIn />', () => {
+  it('should render form', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('form')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sing in now/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
+  })
+})
