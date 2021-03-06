e76b07cedcb1efb244930a5f9b1fe6818dd36a83
Diego Buriti <bs.diego97@gmail.com>
2021-03-06 12:16:25 -0300

    test: add test to render FormSingUp correctly

---
+++ src/components/FormSingUp/test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSingUp from '.'
+
+describe('<FormSingUp />', () => {
+  it('should render form Sign up correctly', () => {
+    const { container } = renderWithTheme(<FormSingUp />)
+
+    expect(screen.getByRole('form')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('password')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/Confirm password/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /Sing Up Now/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSingUp />)
+
+    expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument()
+    expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument()
+  })
+})
