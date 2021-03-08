56261f9d9fadde40da7d1bfad7dba9f14d762b0c
Diego Buriti <bs.diego97@gmail.com>
2021-03-07 22:36:00 -0300

    chore: add prop ref to link correctly

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import { darken } from 'polished'
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
-type WrapperProps = {
+export type WrapperProps = {
   hasIcon: boolean
 } & Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'>
 
