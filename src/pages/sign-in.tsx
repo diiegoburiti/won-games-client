673924e0f246e0344ce3b47d098a0368382b4d33
Diego Buriti <bs.diego97@gmail.com>
2021-03-07 22:37:01 -0300

    chore: create sign-in page

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,10 @@
+import FormSignIn from 'components/FormSignIn'
+import Auth from 'templates/Auth'
+
+export default function singIn() {
+  return (
+    <Auth title="Sing In">
+      <FormSignIn />
+    </Auth>
+  )
+}
