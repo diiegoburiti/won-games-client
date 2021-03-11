300b71c8e3fb61dc60bef990f96ae8b1d48a8f5d
Diego Buriti <bs.diego97@gmail.com>
2021-03-10 21:48:53 -0300

    chore: add intial GameInfo component

---
+++ src/components/GameInfo/index.tsx
@@ -0,0 +1,35 @@
+import {
+  AddShoppingCart,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+import Heading from 'components/Heading'
+import Ribbon from 'components/Ribbon'
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+export type GameInfoProps = {
+  title: string
+  description: string
+  price: string
+}
+
+const GameInfo = ({ title, description, price }: GameInfoProps) => (
+  <S.Wrapper>
+    <Heading color="black" lineBottom>
+      {title}
+    </Heading>
+
+    <Ribbon color="secondary">{`$${price}`}</Ribbon>
+    <span>{description}</span>
+
+    <Button size="large" icon={<AddShoppingCart />}>
+      Add to card
+    </Button>
+    <Button size="large" icon={<FavoriteBorder />} minimal>
+      wishlist
+    </Button>
+  </S.Wrapper>
+)
+
+export default GameInfo
