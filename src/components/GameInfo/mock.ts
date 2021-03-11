1d571073b31e34e93b9c36c388fea8e5bcc63bc1
Diego Buriti <bs.diego97@gmail.com>
2021-03-11 07:21:13 -0300

    chore: add mock for intergration tests

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'GTA V',
+  description:
+    'Aut quae inventore ad expedita, eligendi maxime beatae quas minima numquam atque rem officiis dignissimos explicabo saepe sunt at, sapiente mollitia blanditiis!',
+  price: '199.99'
+}
