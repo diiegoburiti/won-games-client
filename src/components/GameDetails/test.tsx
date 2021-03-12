52e9d59b46b5677140ee266d651ba80df230f1b4
Diego Buriti <bs.diego97@gmail.com>
2021-03-12 07:49:50 -0300

    test: add tests to render GameDetails blocks

---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,42 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameDetails, { GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+  platforms: ['windows', 'linux', 'mac'],
+  developer: 'Rockstar'
+}
+
+describe('<GameDetails />', () => {
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Developer/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /Release Date/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Platforms/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /Rating/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Genres/i })).toBeInTheDocument()
+
+    //expect(container.firstChild).toMatchSnapshot()
+  })
+
+  /*   it('should render platform icons', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByRole('img', { name: /Linux/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Windows/i })).toBeInTheDocument()
+  }) */
+})
