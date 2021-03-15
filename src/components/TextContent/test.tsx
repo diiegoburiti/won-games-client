f3b9564ed5a20f822eace726bd1c7039b118bfbe
Diego Buriti <bs.diego97@gmail.com>
2021-03-14 22:48:37 -0300

    test: add test to render with correctly background mobile and desktop

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -1,28 +1,47 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import TextContent from '.'
 
 const props = {
-  title: 'Gta V',
-  content: '<h1>Description</h1>'
+  title: 'Gta v',
+  content: `<h1>Description</h1>`
 }
 
 describe('<TextContent />', () => {
-  it('should render title and content', () => {
-    renderWithTheme(<TextContent {...props} />)
+  it('should render the title and content', () => {
+    const { container } = renderWithTheme(<TextContent {...props} />)
 
     expect(screen.getByRole('heading', { name: /gta v/i })).toBeInTheDocument()
+
     expect(
-      screen.getByRole('heading', { name: /Description/i })
+      screen.getByRole('heading', { name: /description/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render without title', () => {
-    renderWithTheme(<TextContent content="Description" />)
+    renderWithTheme(<TextContent content={props.content} />)
 
     expect(
       screen.queryByRole('heading', { name: /gta v/i })
     ).not.toBeInTheDocument()
+  })
+
+  it('should render with correctly background', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', { name: /gta v/i })
+      .parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: `${theme.colors.white}` // theme.colors.white
+    })
+
+    expect(wrapper).toHaveStyleRule('color', `${theme.colors.black}`, {
+      media: '(min-width: 768px)'
+    })
   })
 })
