9ac73518f8ade2ef0b36937f671076642c1b8b2c
Diego Buriti <bs.diego97@gmail.com>
2021-03-15 20:53:21 -0300

    test: add test to open and close modal

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,5 +1,5 @@
 import 'match-media-mock'
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import mockItems from './mock'
 import Gallery from '.'
@@ -9,11 +9,30 @@ describe('<Gallery />', () => {
     renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
 
     expect(
-      screen.getByRole('button', { name: /Gallery Image 1/i })
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
     ).toHaveAttribute('src', mockItems[0].src)
 
     expect(
-      screen.getByRole('button', { name: /Gallery Image 2/i })
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
     ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should render open/close modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    const modal = screen.getByLabelText('modal')
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' })
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+    /*
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 }) */
   })
 })
