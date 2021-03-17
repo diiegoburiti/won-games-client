a89e91ffe7b15e6d3f55419496b6a0bbf4a77db0
Diego Buriti <bs.diego97@gmail.com>
2021-03-16 21:51:12 -0300

    style: add styles Game template

---
+++ src/templates/Game/styles.ts
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export type CoverProps = {
+  src: string
+}
+
+export const Cover = styled.div<CoverProps>`
+  ${({ src }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 39.5rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+    ${media.greaterThan('medium')`
+      height: 70rem;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 90%);
+    `}
+  `}
+`
