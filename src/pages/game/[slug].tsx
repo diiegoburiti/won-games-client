a2861cb05e30c7334199ea46e0e55edfc8db1cd8
Diego Buriti <bs.diego97@gmail.com>
2021-03-17 08:10:47 -0300

    chore: add static info

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,4 +1,5 @@
 import Game, { GameTemplateProps } from 'templates/Game'
+import galleryMock from 'components/Gallery/mock'
 
 export default function index(props: GameTemplateProps) {
   return <Game {...props} />
@@ -15,7 +16,14 @@ export async function getStaticProps() {
   return {
     props: {
       cover:
-        'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg'
+        'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+      gameInfo: {
+        title: 'Cyberpunk 2077',
+        price: '59.00',
+        description:
+          'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
+      },
+      gallery: galleryMock
     }
   }
 }
