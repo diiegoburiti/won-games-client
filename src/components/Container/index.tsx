7a4d03b20305827237a27fa55a94005115de3dd1
Diego Buriti <bs.diego97@gmail.com>
2021-03-16 21:48:39 -0300

    style: add width 100% to menu

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   ${({ theme }) => css`
+    width: 100%;
     max-width: ${theme.grid.container};
     margin-left: auto;
     margin-right: auto;
