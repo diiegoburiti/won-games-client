60b0d56c310257b3df804bd5abffe81ab0726bf3
Diego Buriti <bs.diego97@gmail.com>
2021-03-17 08:13:47 -0300

    chore: add gameInfo and gallery props

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,13 +1,29 @@
 import Base from 'templates/Base'
+import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
+
 import * as S from './styles'
+import React from 'react'
 
 export type GameTemplateProps = {
   cover: string
+  gameInfo: GameInfoProps
+  gallery?: GalleryImageProps[]
 }
 
-const Game = ({ cover }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
+    </S.Main>
   </Base>
 )
 
