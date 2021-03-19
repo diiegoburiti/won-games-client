b83e612996b63be999e7d7672d80231853b09af9
Diego Buriti <bs.diego97@gmail.com>
2021-03-19 08:47:45 -0300

    chore: add Empty component to render when where are no games

--- src/templates/WishList/index.tsx
+++ src/templates/WishList/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
 import Heading from 'components/Heading'
@@ -15,7 +16,7 @@ export type WishListTemplateProps = {
 const WishList = ({
   recommendedGames,
   recommendedHighlight,
-  games
+  games = []
 }: WishListTemplateProps) => (
   <Base>
     <Container>
@@ -23,11 +24,19 @@ const WishList = ({
         WishList
       </Heading>
 
-      <Grid>
-        {games?.map((game, index) => (
-          <GameCard key={index} {...game} />
-        ))}
-      </Grid>
+      {games.length ? (
+        <Grid>
+          {games?.map((game, index) => (
+            <GameCard key={index} {...game} />
+          ))}
+        </Grid>
+      ) : (
+        <Empty
+          title="Your wishlist is empty"
+          description="Games added to your wishlist will appear here"
+          hasLink
+        />
+      )}
 
       <Divider />
     </Container>
