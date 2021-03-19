cebe028cecebcbe8ac04d53c67cf69a960bc8bbb
Diego Buriti <bs.diego97@gmail.com>
2021-03-18 22:17:02 -0300

    chore: remove unsed styles

--- src/templates/WishList/index.tsx
+++ src/templates/WishList/index.tsx
@@ -1,11 +1,11 @@
 import { Container } from 'components/Container'
+import { Divider } from 'components/Divider'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
 import ShowCase from 'components/ShowCase'
 import Base from 'templates/Base'
-import * as S from './styles'
 
 export type WishListTemplateProps = {
   recommendedGames: GameCardProps[]
@@ -17,26 +17,26 @@ const WishList = ({
   recommendedHighlight,
   games
 }: WishListTemplateProps) => (
-  <S.Wrapper>
-    <Base>
-      <Container>
-        <Heading lineColor="secondary" lineLeft>
-          WishList
-        </Heading>
+  <Base>
+    <Container>
+      <Heading lineColor="secondary" lineLeft>
+        WishList
+      </Heading>
 
-        <Grid>
-          {games?.map((game, index) => (
-            <GameCard key={index} {...game} />
-          ))}
-        </Grid>
-      </Container>
-      <ShowCase
-        gamesCard={recommendedGames}
-        highlight={recommendedHighlight}
-        title="you may like these games"
-      />
-    </Base>
-  </S.Wrapper>
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={index} {...game} />
+        ))}
+      </Grid>
+
+      <Divider />
+    </Container>
+    <ShowCase
+      gamesCard={recommendedGames}
+      highlight={recommendedHighlight}
+      title="you may like these games"
+    />
+  </Base>
 )
 
 export default WishList
