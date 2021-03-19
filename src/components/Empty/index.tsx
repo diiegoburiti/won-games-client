be0ac1e2716d6966548e71df90d14a1e01b84355
Diego Buriti <bs.diego97@gmail.com>
2021-03-19 08:33:25 -0300

    chore: add Empty component

---
+++ src/components/Empty/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+import Button from 'components/Button'
+
+import * as S from './styles'
+
+export type EmptyProps = {
+  title: string
+  description: string
+  hasLink?: boolean
+}
+
+const Empty = ({ title, description, hasLink }: EmptyProps) => (
+  <S.Wrapper>
+    <S.Image
+      src="/img/empty.svg"
+      alt="a gamer in a couch playing videogame"
+      role="image"
+    />
+
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+
+    {!!hasLink && (
+      <Link href="/" passHref>
+        <Button as="a">Go Back to Store </Button>
+      </Link>
+    )}
+  </S.Wrapper>
+)
+
+export default Empty
