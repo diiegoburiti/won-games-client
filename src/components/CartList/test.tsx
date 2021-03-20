aa56bbcab10d7af0bc92b785eb3786a5a2fa8394
Diego Buriti <bs.diego97@gmail.com>
2021-03-19 21:31:16 -0300

    chore: add CartList component

---
+++ src/components/CartList/test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import mockItems from './mock'
+
+import CartList from '.'
+import theme from 'styles/theme'
+
+describe('<CartList />', () => {
+  it('should render the heading', () => {
+    const { container } = renderWithTheme(
+      <CartList items={mockItems} total="R$ 330,00" />
+    )
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+
+    expect(screen.getByText('R$ 330,00')).toHaveStyle({
+      color: `${theme.colors.primary}`
+    })
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
