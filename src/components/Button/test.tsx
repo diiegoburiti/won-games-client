f2ebb5ac9f966b3276beafc6621f173e89635df7
Diego Buriti <bs.diego97@gmail.com>
2021-03-21 20:18:51 -0300

    test: add a new test to check when button is disabled

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -85,4 +85,16 @@ describe('<Button />', () => {
       '/link'
     )
   })
+
+  it('should render a disabled Button', () => {
+    renderWithTheme(<Button disabled>Buy now</Button>)
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'cursor',
+      'not-allowed',
+      {
+        modifier: ':disabled'
+      }
+    )
+  })
 })
