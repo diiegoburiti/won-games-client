bdf177909088e0db1d8d3563c46559c7d9401e2a
Diego Buriti <bs.diego97@gmail.com>
2021-03-21 21:23:18 -0300

    chore: add new plop generator

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -31,4 +31,31 @@ module.exports = (plop) => {
       }
     ]
   })
+  plop.setGenerator('templates', {
+    description: 'Create a template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your template name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/templates/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
 }
