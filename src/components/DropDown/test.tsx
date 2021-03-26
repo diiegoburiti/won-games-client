56ee93684db4eb6639c7b284b79074908a2b9017
Diego Buriti <bs.diego97@gmail.com>
2021-03-26 08:54:06 -0300

    test: add tests to render DropDown correctly

---
+++ src/components/DropDown/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import DropDown from '.'
+
+describe('<DropDown />', () => {
+  beforeEach(() => {
+    const title = <h5 aria-label="toogle dropdown">Click here</h5>
+
+    const { container } = renderWithTheme(
+      <DropDown title={title}>
+        <span>content</span>
+      </DropDown>
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render title', () => {
+    expect(screen.getByLabelText(/toogle dropdown/)).toBeInTheDocument()
+  })
+
+  it('should handle open/close dropdown', () => {
+    const dropDownContent = screen.getByText(/content/i).parentElement!
+
+    expect(dropDownContent).toHaveStyle({ opacity: 0 })
+    expect(dropDownContent.getAttribute('aria-hidden')).toBe('true')
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(dropDownContent).toHaveStyle({ opacity: 1 })
+    expect(dropDownContent.getAttribute('aria-hidden')).toBe('false')
+  })
+})
