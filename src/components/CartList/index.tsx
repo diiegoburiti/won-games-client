a749676fd6dd2aadf2965e9aa992c4497ca53fbc
Diego Buriti <bs.diego97@gmail.com>
2021-03-26 22:17:12 -0300

    chore: add hasButton  prop

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,20 +1,30 @@
+import Link from 'next/link'
 import GameItem, { GameItemProps } from 'components/GameItem'
+import Button from 'components/Button'
 
 import * as S from './styles'
 
 export type CartListProps = {
   items: GameItemProps[]
   total: string
+  hasButton?: boolean
 }
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
   <S.Wrapper>
     {items.map((item) => (
       <GameItem key={item.title} {...item} />
     ))}
 
     <S.Footer>
-      Total <S.Total>{total}</S.Total>
+      {!hasButton && <span>Total:</span>}
+      <S.Total>{total}</S.Total>
+
+      {hasButton && (
+        <Link href="/cart">
+          <Button as="a">Buy Now</Button>
+        </Link>
+      )}
     </S.Footer>
   </S.Wrapper>
 )
