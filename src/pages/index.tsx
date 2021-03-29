e86ec376d35de7c59799381078420bff9b74657f
Diego Buriti <bs.diego97@gmail.com>
2021-03-29 09:35:04 -0300

    chore: add initial apollo client config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import Home, { HomeTemplateProps } from 'templates/Home'
 
 import bannersMock from 'components/BannerSlider/mock'
@@ -5,9 +6,25 @@ import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
+  const client = new ApolloClient({
+    uri: process.env.graphqlUrL,
+    cache: new InMemoryCache()
+  })
+
+  client.query({
+    query: gql`
+      query getGames {
+        games {
+          name
+        }
+      }
+    `
+  })
+
   return <Home {...props} />
 }
 
+console.log(process.env.graphqlUrL)
 export function getServerSideProps() {
   return {
     props: {
