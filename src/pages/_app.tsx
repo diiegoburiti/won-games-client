b500da3a282d506fe4cd7274a4e859dcbf33ab07
Diego Buriti <bs.diego97@gmail.com>
2021-03-30 07:23:15 -0300

    chore: add global apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,36 @@
-import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
+import { AppProps } from 'next/app'
 
+import { ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: process.env.graphqlUrL,
+    cache: new InMemoryCache()
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,12 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import Home, { HomeTemplateProps } from 'templates/Home'
-
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
-  const client = new ApolloClient({
-    uri: process.env.graphqlUrL,
-    cache: new InMemoryCache()
-  })
-
-  client.query({
-    query: gql`
-      query getGames {
-        games {
-          name
-        }
-      }
-    `
-  })
-
   return <Home {...props} />
 }
 
-console.log(process.env.graphqlUrL)
 export function getServerSideProps() {
   return {
     props: {
