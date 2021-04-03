017c555f6758aba978d02336c939bf2d46b27f60
Diego Buriti <bs.diego97@gmail.com>
2021-04-03 15:51:01 -0300

    chore: should return Free when price is equal 0

--- src/utils/forma-price.ts
+++ src/utils/forma-price.ts
@@ -1,4 +1,5 @@
 export default function formatPrice(price: number | bigint): string {
+  if (price === 0) return 'Free'
   return new Intl.NumberFormat('en', {
     style: 'currency',
     currency: 'USD'
