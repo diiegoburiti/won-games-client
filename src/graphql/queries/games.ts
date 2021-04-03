82c9a48786b117337afbff1c4665acf5f5ca4a64
Diego Buriti <bs.diego97@gmail.com>
2021-04-03 10:44:46 -0300

    chore: add query QUERY_GAMES_BY_SLUG

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -16,3 +16,35 @@ export const QUERY_GAMES = gql`
     }
   }
 `
+
+export const QUERY_GAMES_BY_SLUG = gql`
+  query QueryGamesBySlug($slug: String!) {
+    games(where: { slug: $slug }) {
+      name
+      short_description
+      description
+      price
+      rating
+      release_date
+      categories {
+        name
+      }
+      gallery {
+        src: url
+        label: alternativeText
+      }
+      cover {
+        src: url
+      }
+      developers {
+        name
+      }
+      publisher {
+        name
+      }
+      platforms {
+        name
+      }
+    }
+  }
+`
