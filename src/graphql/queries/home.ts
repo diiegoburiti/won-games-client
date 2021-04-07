8f5c0d0268403c7d6c4fdc9209cde08f540d5ea5
Diego Buriti <bs.diego97@gmail.com>
2021-04-07 07:48:14 -0300

    chore: add upcomingGames fragment

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -8,9 +8,18 @@ export const QUERY_HOME = gql`
     banners {
       ...BannerFragment
     }
+
     newGames: games(
       where: { release_date_lte: "2021-01-27" }
       sort: "release_date:desc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
+
+    upcomingGames: games(
+      where: { release_date_gt: "2021-01-27" }
+      sort: "release_date:asc"
       limit: 8
     ) {
       ...GameFragment
