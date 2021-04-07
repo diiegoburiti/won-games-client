f783046e592effbb3ddbe000c522e9991dc56b75
Diego Buriti <bs.diego97@gmail.com>
2021-04-07 08:59:08 -0300

    chore: add dinamic popularGames

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import { QueryHome } from 'graphql/generated/QueryHome'
 
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
 export default function Index(props: HomeTemplateProps) {
@@ -16,7 +15,7 @@ export async function getStaticProps() {
   const apolloClient = initializeApollo()
 
   const {
-    data: { banners, newGames, upcomingGames, freeGames }
+    data: { banners, newGames, upcomingGames, freeGames, sections }
   } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
 
   return {
@@ -42,7 +41,13 @@ export async function getStaticProps() {
         price: game.price
       })),
       mostPopularHighlight: highlightMock,
-      mostPopularGames: gamesMock,
+      mostPopularGames: sections?.popularGames!.games.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: game.price
+      })),
       upcomingGames: upcomingGames.map((game) => ({
         title: game.name,
         slug: game.slug,
