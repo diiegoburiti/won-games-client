c050bb25c45e87f73476f714b52b8cf205a2acb5
Diego Buriti <bs.diego97@gmail.com>
2021-04-11 19:58:24 -0300

    chore: add QUERY_RECOMMENDED on wishlist router

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,16 +1,30 @@
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+
 import WishList, { WishListTemplateProps } from 'templates/WishList'
 import gamesMock from 'components/GameCardSlider/mock'
-import HighlightMock from 'components/Highlight/mock'
+
+import { gamesMapper, highlightMapper } from 'utils/mappers'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 
 export default function WishListPage(props: WishListTemplateProps) {
   return <WishList {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
-      recommendedGames: gamesMock.slice(0, 5),
-      recommendedHighlight: HighlightMock,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedTitle: data.recommended?.section?.title,
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      ),
       games: gamesMock
     }
   }
