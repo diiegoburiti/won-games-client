17205f681af942d937802f948e4ef00add395e89
Diego Buriti <bs.diego97@gmail.com>
2021-05-07 08:57:23 -0300

    chore: add CartProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { useApollo } from 'utils/apollo'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { CartProvider } from 'hooks/use-cart'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -14,18 +15,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Won Games</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <CartProvider>
+          <Head>
+            <title>Won Games</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta
+              name="description"
+              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </CartProvider>
       </ThemeProvider>
     </ApolloProvider>
   )
