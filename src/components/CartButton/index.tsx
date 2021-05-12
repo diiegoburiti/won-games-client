099fc6701478277c8062d9a631867e21b9f50bbe
Diego Buriti <bs.diego97@gmail.com>
2021-05-12 08:06:02 -0300

    chore: add two new props

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -2,15 +2,23 @@ import {
   AddShoppingCart,
   RemoveShoppingCart
 } from '@styled-icons/material-outlined'
-import Button from 'components/Button'
+import Button, { ButtonProps } from 'components/Button'
 import { useCart } from 'hooks/use-cart'
+import React from 'react'
 
 type CartButtonProps = {
   id: string
-}
+  hasText?: boolean
+} & Pick<ButtonProps, 'size'>
 
-const CartButton = ({ id }: CartButtonProps) => {
+const CartButton = ({
+  id,
+  hasText = false,
+  size = 'small'
+}: CartButtonProps) => {
   const { isInCart, addToCart, removeFromCart } = useCart()
+
+  const buttonText = isInCart(id) ? 'Remove From Cart' : 'Add To Cart'
 
   function handleClick() {
     isInCart(id) ? removeFromCart(id) : addToCart(id)
@@ -18,6 +26,7 @@ const CartButton = ({ id }: CartButtonProps) => {
 
   return (
     <Button
+      size={size}
       icon={
         isInCart(id) ? (
           <RemoveShoppingCart aria-label="Remove from cart" />
@@ -25,9 +34,10 @@ const CartButton = ({ id }: CartButtonProps) => {
           <AddShoppingCart aria-label="Add to cart" />
         )
       }
-      size="small"
       onClick={handleClick}
-    />
+    >
+      {hasText && buttonText}
+    </Button>
   )
 }
 
