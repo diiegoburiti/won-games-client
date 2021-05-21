5427edb6253318a4b40c65a6425ac7bb1972bc9e
Diego Buriti <bs.diego97@gmail.com>
2021-05-21 08:16:54 -0300

    style: add cursor pointer

--- src/components/UserDropDown/styles.ts
+++ src/components/UserDropDown/styles.ts
@@ -26,6 +26,7 @@ export const Link = styled.a`
     color: ${theme.colors.black};
     padding: ${theme.spacings.xsmall} ${theme.spacings.small};
     transition: background, color, ${theme.transition.default};
+    cursor: pointer;
     &:hover {
       background: ${theme.colors.primary};
       color: ${theme.colors.white};
