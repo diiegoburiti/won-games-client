7415d2a61a589ae746ca0fa627bd25f22e19060b
Diego Buriti <bs.diego97@gmail.com>
2021-05-21 08:32:53 -0300

    chore: add sign-up method

--- src/components/FormSingUp/index.tsx
+++ src/components/FormSingUp/index.tsx
@@ -1,49 +1,98 @@
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { signin } from 'next-auth/client'
+import { useMutation } from '@apollo/client'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import { MUTATION_REGISTER } from 'graphql/mutations/register'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
 import TextField from 'components/TextField'
 import Button from 'components/Button'
-import { FormLink, FormWrapper } from 'components/Form'
+import { FormLink, FormWrapper, FormLoading } from 'components/Form'
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form aria-label="sing-up form">
-      <TextField
-        name="name"
-        placeholder="name"
-        type="text"
-        icon={<AccountCircle />}
-      />
-      <TextField
-        name="email"
-        placeholder="email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="password"
-        type="password"
-        icon={<Lock />}
-      />
-      <TextField
-        name="confirm-password"
-        placeholder="Confirm password"
-        type="password"
-        icon={<Lock />}
-      />
+const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    email: '',
+    password: '',
+    username: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign Up Now
-      </Button>
+  const [createrUser, { error, loading }] = useMutation(MUTATION_REGISTER, {
+    onError: (err) => console.error(err),
+    onCompleted: () => {
+      !error &&
+        signin('credentials', {
+          email: values.email,
+          password: values.password,
+          callbackUrl: '/'
+        })
+    }
+  })
 
-      <FormLink>
-        Already have an account?
-        <Link href="/sign-in">
-          <a>Sign in</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  function handleInputChange(field: string, value: string) {
+    setValues((oldState) => ({ ...oldState, [field]: value }))
+  }
+
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault()
+
+    createrUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  return (
+    <FormWrapper>
+      <form aria-label="sing-up form" onSubmit={handleSubmit}>
+        <TextField
+          name="username"
+          placeholder="Username"
+          type="text"
+          icon={<AccountCircle />}
+          onInputChange={(value) => handleInputChange('username', value)}
+        />
+        <TextField
+          name="email"
+          placeholder="email"
+          type="email"
+          icon={<Email />}
+          onInputChange={(value) => handleInputChange('email', value)}
+        />
+        <TextField
+          name="password"
+          placeholder="password"
+          type="password"
+          icon={<Lock />}
+          onInputChange={(value) => handleInputChange('password', value)}
+        />
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+          onInputChange={(value) =>
+            handleInputChange('confirm-password', value)
+          }
+        />
+
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Sign up now</span>}
+        </Button>
+
+        <FormLink>
+          Already have an account?
+          <Link href="/sign-in">
+            <a>Sign in</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
