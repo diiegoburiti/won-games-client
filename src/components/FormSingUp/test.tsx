e4d0d781da67f95e9d2422e62d38509ba2412192
Diego Buriti <bs.diego97@gmail.com>
2021-05-21 08:34:51 -0300

    test: add mocks MockedProvider and useRouter

--- src/components/FormSingUp/test.tsx
+++ src/components/FormSingUp/test.tsx
@@ -1,11 +1,27 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { screen } from '@testing-library/react'
 import { render } from 'utils/test-utils'
 
 import FormSingUp from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 describe('<FormSingUp />', () => {
   it('should render form Sign up correctly', () => {
-    const { container } = render(<FormSingUp />)
+    const { container } = render(
+      <MockedProvider mocks={[]}>
+        <FormSingUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByRole('form')).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
@@ -20,7 +36,11 @@ describe('<FormSingUp />', () => {
   })
 
   it('should render text to sign up if already have an account', () => {
-    render(<FormSingUp />)
+    render(
+      <MockedProvider mocks={[]}>
+        <FormSingUp />
+      </MockedProvider>
+    )
 
     expect(screen.getByRole('link', { name: /sign in/i })).toBeInTheDocument()
     expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument()
