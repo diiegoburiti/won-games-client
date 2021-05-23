f184d028356a385754a443ba4906bb69e0655b29
Diego Buriti <bs.diego97@gmail.com>
2021-05-23 10:03:36 -0300

    test: add tests to singIn and singUp

---
+++ src/utils/validation/test.ts
@@ -0,0 +1,64 @@
+import { signInValidate, signUpValidate } from '.'
+
+describe('validations', () => {
+  describe('signInValidate', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '', password: '' }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty',
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'sasasas', password: 'admin123' }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" must be a valid email'
+      })
+    })
+  })
+
+  describe('signUpValidate', () => {
+    it('should validate empty fields', () => {
+      const values = { username: '', email: '', password: '' }
+
+      expect(signUpValidate(values)).toMatchObject({
+        email: expect.any(String),
+        username: expect.any(String),
+        password: expect.any(String),
+        confirm_password: expect.any(String)
+      })
+    })
+
+    it('should return short username error', () => {
+      const values = { username: 'hik', email: '', password: '' }
+
+      expect(signUpValidate(values).username).toMatchInlineSnapshot(
+        `"\\"username\\" length must be at least 5 characters long"`
+      )
+    })
+
+    it('should return invalid email error', () => {
+      const values = { username: 'hik', email: 'sasasas', password: 'admin123' }
+
+      expect(signUpValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+
+    it('should return error if password does not match with confirm password', () => {
+      const values = {
+        username: 'admin',
+        email: 'admin@gmail.com',
+        password: 'admin123',
+        confirm_password: 'admin1234'
+      }
+
+      expect(signUpValidate(values).confirm_password).toMatchInlineSnapshot(
+        `"confirm password does not match with password"`
+      )
+    })
+  })
+})
