9ebd383c4f225f2baefccfac73aaf679de543e47
Diego Buriti <bs.diego97@gmail.com>
2021-05-24 08:15:13 -0300

    style: add FormError

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -40,3 +40,14 @@ export const FormLoading = styled.img.attrs(() => ({
 }))`
   width: 4rem;
 `
+
+export const FormError = styled.div`
+  ${({ theme }) => css`
+    text-align: center;
+    color: red;
+    font-size: ${theme.font.sizes.small};
+    svg {
+      width: 1.6rem;
+    }
+  `}
+`
