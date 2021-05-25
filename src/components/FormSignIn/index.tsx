dbcd2ee1a4bdf12e4d01b21477e189e064ea7a20
Diego Buriti <bs.diego97@gmail.com>
2021-05-25 08:00:17 -0300

    chore: add callbackUrl to redirect flow

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -15,7 +15,8 @@ const FormSignIn = () => {
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState({ email: '', password: '' })
   const [loading, setLoading] = useState(false)
-  const { push } = useRouter()
+  const routes = useRouter()
+  const { push, query } = routes
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
@@ -38,7 +39,7 @@ const FormSignIn = () => {
     const result = await signIn('credentials', {
       ...values,
       redirect: false,
-      callbackUrl: '/'
+      callbackUrl: `${window.location.origin}${query.callbackUrl || ''}`
     })
 
     if (result?.url) {
