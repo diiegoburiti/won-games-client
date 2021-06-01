b6826f37b0d4545c73b6df1f7f2b610f1cb11c3f
Diego Buriti <bs.diego97@gmail.com>
2021-06-01 08:31:55 -0300

    chore: add reset password flow

--- src/components/FormResetPassword/index.tsx
+++ src/components/FormResetPassword/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/client'
 import { useRouter } from 'next/router'
+
 import { Lock, ErrorOutline } from '@styled-icons/material-outlined'
+
 import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
+
 import { FieldErrors, resetValidate } from 'utils/validations'
 
 const FormResetPassword = () => {
@@ -12,8 +15,7 @@ const FormResetPassword = () => {
   const [fieldError, setFieldError] = useState<FieldErrors>({})
   const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
-  const routes = useRouter()
-  const { push, query } = routes
+  const { query } = useRouter()
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }))
@@ -33,19 +35,33 @@ const FormResetPassword = () => {
 
     setFieldError({})
 
-    const result = await signIn('credentials', {
-      ...values,
-      redirect: false,
-      callbackUrl: `${window.location.origin}${query.callbackUrl || ''}`
-    })
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          password: values.password,
+          passwordConfirmation: values.confirm_password,
+          code: query.code
+        })
+      }
+    )
 
-    if (result?.url) {
-      return push(result?.url)
+    const data = await response.json()
+
+    if (data.error) {
+      setFormError(data.message[0].messages[0].message)
+      setLoading(false)
+    } else {
+      signIn('credentials', {
+        email: data.user.email,
+        password: values.password,
+        callbackUrl: '/'
+      })
     }
-
-    setLoading(false)
-
-    setFormError('username or password is invalid!')
   }
 
   return (
@@ -55,27 +71,26 @@ const FormResetPassword = () => {
           <ErrorOutline /> {formError}
         </FormError>
       )}
-      <form aria-label="sign-in form" onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <TextField
           name="password"
-          placeholder=" Type your new password"
+          placeholder="Password"
           type="password"
           errorMessage={fieldError?.password}
           onInputChange={(v) => handleInput('password', v)}
           icon={<Lock />}
         />
-
         <TextField
           name="confirm_password"
-          placeholder="Repeat your password"
+          placeholder="Confirm password"
           type="password"
-          errorMessage={fieldError?.password}
+          errorMessage={fieldError?.confirm_password}
           onInputChange={(v) => handleInput('confirm_password', v)}
           icon={<Lock />}
         />
 
         <Button type="submit" size="large" fullWidth disabled={loading}>
-          {loading ? <FormLoading /> : <span>Reset password</span>}
+          {loading ? <FormLoading /> : <span>Reset Password</span>}
         </Button>
       </form>
     </FormWrapper>
---
+++ src/components/FormResetPassword/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import FormResetPassword from '.'
+
+export default {
+  title: 'FormResetPassword',
+  component: FormResetPassword
+} as Meta
+
+export const Default: Story = () => <FormResetPassword />
