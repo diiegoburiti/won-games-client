ea77393d48d58d9ea3c668eea0f4d18660a05582
Diego Buriti <bs.diego97@gmail.com>
2021-06-07 08:35:24 -0300

    chore: chore add Wishlist provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,11 @@ import { AppProps } from 'next/app'
 import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from 'utils/apollo'
-
 import { ThemeProvider } from 'styled-components'
+import { CartProvider } from 'hooks/use-cart'
+import { WishlistProvider } from 'hooks/use-wishlist'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
-import { CartProvider } from 'hooks/use-cart'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -19,24 +19,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+                />
+              </Head>
+              <GlobalStyles />
+              <Component {...pageProps} />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={4}
               />
-            </Head>
-            <GlobalStyles />
-            <Component {...pageProps} />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={4}
-            />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
