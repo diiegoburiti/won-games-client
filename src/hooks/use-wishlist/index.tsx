d2db46ae55d9b67bb43d2ce68b84f4bcfde7cba0
Diego Buriti <bs.diego97@gmail.com>
2021-06-11 08:57:03 -0300

    fix: return updateList

--- src/hooks/use-wishlist/index.tsx
+++ src/hooks/use-wishlist/index.tsx
@@ -102,7 +102,7 @@ const WishlistProvider = ({ children }: WishlistProviderProps) => {
   }
 
   const removeFromWishlist = (id: string) => {
-    updateList({
+    return updateList({
       variables: {
         input: {
           where: { id: wishlistId },
