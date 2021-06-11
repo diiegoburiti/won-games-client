0218b2d110b8a7a22cab9c04ba35350ad41db4dd
Diego Buriti <bs.diego97@gmail.com>
2021-06-11 09:05:54 -0300

    chore: add  WishlistButton

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link'
-
-import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
-
-import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import CartButton from 'components/CartButton'
-
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
+import WishlistButton from 'components/WishlistButton'
 import formatPrice from 'utils/forma-price'
 import * as S from './styles'
 
@@ -16,7 +13,6 @@ export type GameCardProps = {
   img: string
   price: number
   promotionalPrice?: number
-  favorite?: boolean
   ribbon?: React.ReactNode
   ribbonColor?: RibbonColors
   ribbonSize?: RibbonSizes
@@ -31,11 +27,9 @@ const GameCard = ({
   img,
   price,
   promotionalPrice,
-  favorite = false,
   ribbon,
   ribbonColor = 'primary',
-  ribbonSize = 'small',
-  onFav
+  ribbonSize = 'small'
 }: GameCardProps) => (
   <S.Wrapper>
     {!!ribbon && (
@@ -55,12 +49,8 @@ const GameCard = ({
           <S.Developer>{developer}</S.Developer>
         </S.Info>
       </Link>
-      <S.FavButton onClick={onFav} role="button">
-        {favorite ? (
-          <Favorite aria-label="Remove from Wishlist" />
-        ) : (
-          <FavoriteBorder aria-label="Add to Wishlist" />
-        )}
+      <S.FavButton>
+        <WishlistButton id={id} />
       </S.FavButton>
       <S.BuyBox>
         {!!promotionalPrice && (
