75cc745ec0f49404a07f814e8c80dc943605256b
Diego Buriti <bs.diego97@gmail.com>
2021-06-18 07:21:57 -0300

    refactor: protecting cart router

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,20 +1,20 @@
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
-
 import Cart, { CartProps } from 'templates/Cart'
-
 import itemsMock from 'components/CartList/mock'
 import cardsMock from 'components/PaymentOptions/mock'
-
 import { initializeApollo } from 'utils/apollo'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
+import protectedRoutes from 'utils/protected-routes'
+import { GetServerSidePropsContext } from 'next'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
@@ -22,6 +22,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      session,
       items: itemsMock,
       total: '$ 430,00',
       cards: cardsMock,
