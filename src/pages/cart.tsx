5cda767f481325dcb3d0399d4de4aa8ae94799ad
Diego Buriti <bs.diego97@gmail.com>
2021-06-22 07:17:56 -0300

    refactor: remove mocks

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,10 @@
+import { GetServerSidePropsContext } from 'next'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import Cart, { CartProps } from 'templates/Cart'
-import itemsMock from 'components/CartList/mock'
 import { initializeApollo } from 'utils/apollo'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
 import protectedRoutes from 'utils/protected-routes'
-import { GetServerSidePropsContext } from 'next'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
@@ -22,9 +21,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
-      items: itemsMock,
-      total: '$ 430,00',
-      cards: cardsMock,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
