330930c04c47e6c73781a083f6b122048138cd56
Diego Buriti <bs.diego97@gmail.com>
2021-06-22 07:31:00 -0300

    style: add custom styles to CardElement

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -19,7 +19,14 @@ const PaymentForm = () => {
           Payment
         </Heading>
         <CardElement
-          options={{ hidePostalCode: true }}
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px'
+              }
+            }
+          }}
           onChange={handleChange}
         />
 
