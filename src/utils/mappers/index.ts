0aa88660e088b119001f9659db8d3229a320032d
Diego Buriti <bs.diego97@gmail.com>
2021-07-02 07:52:55 -0300

    chore: add method ordersMapper

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -3,6 +3,7 @@ import {
   QueryHome_banners,
   QueryHome_sections_freeGames_highlight
 } from 'graphql/generated/QueryHome'
+import { QueryOrders_orders } from 'graphql/generated/QueryOrders'
 import { QueryWishlist_wishlists_games } from 'graphql/generated/QueryWishlist'
 import formatPrice from 'utils/forma-price'
 
@@ -60,5 +61,35 @@ export const cartMapper = (games: QueryGames_games[] | null | undefined) => {
         price: formatPrice(game.price),
         title: game.name
       }))
+    : []
+}
+
+export const ordersMapper = (orders: QueryOrders_orders[] | undefined) => {
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(order.created_at))}`
+          },
+          games: order.games.map((game) => ({
+            id: game.id,
+            title: game.name,
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: `http://localhost:1337${game.cover?.url}`,
+            price: formatPrice(game.price)
+          }))
+        }
+      })
     : []
 }
