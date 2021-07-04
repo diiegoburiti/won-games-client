ec93fe51c1019110b148f90fb6ad07256e721bb4
Diego Buriti <bs.diego97@gmail.com>
2021-07-04 20:21:11 -0300

    feat: add orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,8 +1,14 @@
 import { GetServerSidePropsContext } from 'next'
 import Profile from 'templates/Profile'
 import OrdersList, { OrdersListProps } from 'components/OrderList'
-import ordersMock from 'components/OrderList/mock'
 import protectedRoutes from 'utils/protected-routes'
+import { initializeApollo } from 'utils/apollo'
+import {
+  QueryOrders,
+  QueryOrdersVariables
+} from 'graphql/generated/QueryOrders'
+import { QUERY_ORDERS } from 'graphql/queries/orders'
+import { ordersMapper } from 'utils/mappers'
 
 export default function ProfileCards({ items }: OrdersListProps) {
   return (
@@ -14,10 +20,17 @@ export default function ProfileCards({ items }: OrdersListProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
 
+  const { data } = await apolloClient.query<QueryOrders, QueryOrdersVariables>({
+    query: QUERY_ORDERS,
+    variables: {
+      identifier: session?.id
+    }
+  })
   return {
     props: {
-      items: ordersMock,
+      items: ordersMapper(data.orders),
       session
     }
   }
