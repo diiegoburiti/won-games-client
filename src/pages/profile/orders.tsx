1b7cf943b8c3dfa7c083cac2e73bea4912b8a3d6
Diego Buriti <bs.diego97@gmail.com>
2021-07-06 08:19:10 -0300

    chore: prevent to get cache from orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -26,7 +26,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     query: QUERY_ORDERS,
     variables: {
       identifier: session?.id
-    }
+    },
+    fetchPolicy: 'no-cache'
   })
   return {
     props: {
