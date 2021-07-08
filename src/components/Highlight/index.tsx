6c3596a93d6e1fd7be3da939b6312b9bdd5f4737
Diego Buriti <bs.diego97@gmail.com>
2021-07-08 08:08:24 -0300

    chore: update Highlight to use next/image

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Button from 'components/Button'
 import * as S from './styles'
 
@@ -20,8 +21,13 @@ const Highlight = ({
   buttonLink,
   alignment = 'right'
 }: HighlightProps) => (
-  <S.Wrapper alignment={alignment} backgroundImage={backgroundImage}>
-    {!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
+  <S.Wrapper alignment={alignment}>
+    <Image src={backgroundImage} alt={`${title} background`} layout="fill" />
+    {!!floatImage && (
+      <S.FloatImage>
+        <Image src={floatImage} alt={title} layout="fill" objectFit="cover" />
+      </S.FloatImage>
+    )}
     <S.Content>
       <S.Title>{title}</S.Title>
       <S.SubTitle>{subTitle}</S.SubTitle>
