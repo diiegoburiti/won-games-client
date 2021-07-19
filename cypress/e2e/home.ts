db9ec5f91912ab3c6dbb6ed34a1be339762db08e
Diego Buriti <bs.diego97@gmail.com>
2021-07-19 17:55:47 -0300

    test: using click and wait to animation on banner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -1,5 +1,26 @@
+/// <reference path="../support/index.d.ts" />
+
 describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
+
+    cy.get('.slick-slider').within(() => {
+      cy.findAllByText(/kingdom come/i )
+      cy.findAllByText(/buy now/i)
+
+
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findAllByText(/Cyberpunk 2077/i)
+      cy.findAllByText(/buy now/i)
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findAllByText(/Dead Cells/i )
+      cy.findAllByText(/buy now/i)
+    })
   });
 });
