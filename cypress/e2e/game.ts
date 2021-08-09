67140d02550be30df36dfc9592fb114db981ffac
Diego Buriti <bs.diego97@gmail.com>
2021-08-08 21:04:48 -0300

    test: search for details and showcase inside GamePage

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,20 +1,39 @@
-/// <reference path="../support/index.d.ts" /> 
+/// <reference path="../support/index.d.ts" />
 
 describe('Game Page', () => {
   it('should render game page sections', () => {
     cy.visit('/game/cyberpunk-2077')
 
     cy.getByDataCy('game-info').within(() => {
-      cy.findByRole('heading', {name: /Cyberpunk 2077/i}).should('exist')
+      cy.findByRole('heading', { name: /Cyberpunk 2077/i }).should('exist')
       cy.findByText(/Cyberpunk 2077 is an open-world/i).should('exist')
       cy.findByText('$199.90').should('exist')
-      cy.findByRole('button', {name: /add to cart/i}).should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
     })
 
     cy.getByDataCy('content').within(() => {
-      cy.findByRole('heading', {name: /description/i}).should('exist')
-
+      cy.findByRole('heading', { name: /description/i }).should('exist')
     })
-      cy.getByDataCy('content').children().should('have.length.at.least', 2)
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist') 
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
+      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Role-playing / Action / Sci-fi').should('exist')
+    })
+
+      cy.shouldRenderShowcase({name: 'Upcoming Games', highlight: true})
+      cy.shouldRenderShowcase({name: 'Recommended Games', highlight: true})
   })
 })
