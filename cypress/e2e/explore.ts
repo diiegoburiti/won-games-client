2b6615fa4d71a444707da2dfac144893c1eff110
Diego Buriti <bs.diego97@gmail.com>
2021-08-14 17:38:58 -0300

    test: add sort by price test

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -24,4 +24,26 @@ describe('Explore Page', () => {
     cy.getByDataCy('game-card').should('have.length', 30)
 
   })
+
+  it('should order by price', () => {
+    cy.findAllByText(/lowest to highest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText(/free/i).should('exist')
+    })
+
+    cy.findAllByText(/highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+
+    cy.getByDataCy('game-card').first().within(() => {
+      cy
+      .findByText(/^\$\d+(\.\d{1,2})?/)
+      .invoke('text')
+      .then($el => $el.replace('$', ''))
+      .then(parseFloat)
+      .should('be.gt', 0)
+    })
+
+  })
 })
