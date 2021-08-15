977a9d851f3c032a0a1f56a327e73f5d2560d02f
Diego Buriti <bs.diego97@gmail.com>
2021-08-14 21:07:22 -0300

    test: add test to sort by price

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -26,19 +26,58 @@ describe('Explore Page', () => {
   })
 
   it('should order by price', () => {
-    cy.findAllByText(/lowest to highest/i).click()
+    cy.findByText(/lowest to highest/i).click()
     cy.location('href').should('contain', 'sort=price%3Aasc')
 
     cy.getByDataCy('game-card').first().within(() => {
       cy.findByText(/free/i).should('exist')
     })
 
-    cy.findAllByText(/highest to lowest/i).click()
+    cy.findByText(/highest to lowest/i).click()
     cy.location('href').should('contain', 'sort=price%3Adesc')
 
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeGreaterThan(0)
     })
+  })
 
+  it('should filter by price', () => {
+    cy.findByText(/highest to lowest/i).click()
+
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText(/free/i).should('exist')
+    })
+
+    cy.findByText('Under $50').click()
+    cy.location('href').should('contain', 'price_lte=50')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(50)
+    })
+
+    cy.findByText('Under $100').click()
+    cy.location('href').should('contain', 'price_lte=100')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(100)
+    })
+
+    cy.findByText('Under $150').click()
+    cy.location('href').should('contain', 'price_lte=150')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(150)
+    })
+
+    cy.findByText('Under $250').click()
+    cy.location('href').should('contain', 'price_lte=250')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(250)
+    })
+
+    cy.findByText('Under $500').click()
+    cy.location('href').should('contain', 'price_lte=500')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(500)
+    })
   })
 }) 
