92d83a0750afe8e9296364ae45f728f3564a83c5
Diego Buriti <bs.diego97@gmail.com>
2021-08-18 21:18:32 -0300

    refactor: refactor SingUp test to use signUp custom command

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -8,11 +8,7 @@ describe('User', () => {
 
     cy.visit('/sign-up')
 
-    cy.findByPlaceholderText(/username/i).type(user.username)
-    cy.findByPlaceholderText(/email/i).type(user.email)
-    cy.findByPlaceholderText(/^password/i).type(user.password)
-    cy.findByPlaceholderText(/confirm password/i).type(user.password)
-    cy.findByRole('button', {name: /sign up now/i}).click()
+    cy.signUp(user)
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(`${user.username}`).should('exist')
