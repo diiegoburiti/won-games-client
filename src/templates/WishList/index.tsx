462092f3af5852776b670e9af81e82f6583767ef
Diego Buriti <bs.diego97@gmail.com>
2021-08-24 08:00:51 -0300

    test: add wishlist data-cy

--- src/templates/WishList/index.tsx
+++ src/templates/WishList/index.tsx
@@ -26,7 +26,7 @@ const WishList = ({
 
   return (
     <Base>
-      <Container>
+      <Container data-cy="wishlist">
         <Heading lineLeft lineColor="secondary">
           Wishlist
         </Heading>
