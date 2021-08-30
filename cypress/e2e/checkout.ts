3c377c9b9b7fedd085c01108b1730867104105af
Diego Buriti <bs.diego97@gmail.com>
2021-08-30 08:47:29 -0300

    test: add paid games e2e test

--- cypress/e2e/checkout.ts
+++ cypress/e2e/checkout.ts
@@ -1,5 +1,6 @@
  /// <reference path="../support/index.d.ts" /> 
- import { create } from "cypress/types/lodash";
+ import { should } from "chai";
+import { create } from "cypress/types/lodash";
 import { createUser, User } from "../support/generate";
 
 describe('Checkout', () => {
@@ -44,6 +45,54 @@ describe('Checkout', () => {
 
       cy.signIn(user.email, user.password)
       cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`)
+      cy.getByDataCy('game-item').should('have.length', 1)
+    })
+  })
+
+  describe('Paid games', () => {
+    before(() => {
+      user = createUser()
+    })
+
+    it('should buy paid games', () => {
+      cy.visit('/sign-up')
+      cy.signUp(user)
+      cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+      cy.findByRole('button', {name: /explore/i}).click()
+      cy.url().should('eq', `${Cypress.config().baseUrl}/games`)
+
+      cy.findByText(/highest to lowest/i).click()
+      cy.location('href').should('contain', 'sort=price%3Adesc')
+
+      cy.addToCartByIndex(0)
+
+      cy.findAllByLabelText(/cart items/i).first().should('have.text', ' 1').click()
+
+      cy.getByDataCy('cart-list').within(() => {
+        cy.findByText(/buy it now/i).click()
+      })
+
+      cy.findByRole('button', {name: /buy now/i}).should('have.attr', 'disabled')
+
+      cy.fillElementsInput('cardNumber', '4242424242424242')
+      cy.fillElementsInput('cardExpiry', '1040')
+      cy.fillElementsInput('cardCvc', '103')
+
+      cy.findByRole('button', {name: /buy now/i}).click()
+
+      cy.url().should('eq', `${Cypress.config().baseUrl}/success`)
+
+      cy.findByRole('heading', { name: /Your purchase was successful!/i }).should('exist')
+    })
+
+    it('should show games in order page', () => {
+      cy.visit('/profile/orders')
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/orders`)
+
+      cy.signIn(user.email, user.password)
+      cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/orders`)
+
       cy.getByDataCy('game-item').should('have.length', 1)
     })
   })
